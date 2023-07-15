65fbeea7946756682da3dbdbc8694ef298c30b99
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-15 14:41:08 +0500

    feat(auth): created all endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,28 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { LoginDto, SignupDto } from './dto';
 import { AuthService } from './auth.service';
+import { LoginDto, SignupDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @Post('login')
-  login(@Body() dto: LoginDto) {
-    return this.authService.login(dto);
+  @Post('local/signup')
+  signupLocal(@Body() dto: SignupDto) {
+    return this.authService.signupLocal(dto);
   }
 
-  @Post('signup')
-  signup(@Body() dto: SignupDto) {
-    return this.authService.signup(dto);
+  @Post('local/login')
+  loginLocal(@Body() dto: LoginDto) {
+    return this.authService.loginLocal(dto);
+  }
+
+  @Post('/logout')
+  logout() {
+    return this.authService.logout();
+  }
+
+  @Post('/refresh')
+  refresh() {
+    return this.authService.logout();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { LoginDto, SignupDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  login(dto: LoginDto) {
+  constructor(private prismaService: PrismaService) {}
+
+  loginLocal(dto: LoginDto) {
     return 'logged in ' + dto.email;
   }
 
-  signup(dto: SignupDto) {
+  signupLocal(dto: SignupDto) {
     return 'signed up ' + dto.email;
+  }
+
+  logout() {
+    return 0;
+  }
+
+  refresh() {
+    return 0;
   }
 }

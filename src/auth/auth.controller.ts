a3e788a20cbeda3ec108fb01af84765b3c8e9fb0
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-15 18:08:42 +0500

    feat(prisma): fully implemented signup and login logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto, SignupDto } from './dto';
+import { Tokens } from './types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('local/signup')
-  signupLocal(@Body() dto: SignupDto) {
+  signupLocal(@Body() dto: SignupDto): Promise<Tokens> {
     return this.authService.signupLocal(dto);
   }
 
   @Post('local/login')
-  loginLocal(@Body() dto: LoginDto) {
+  loginLocal(@Body() dto: LoginDto): Promise<Tokens> {
     return this.authService.loginLocal(dto);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { LoginDto, SignupDto } from './dto';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-  hashData(data: string): Promise<string> {
-    return bcrypt.hash(data, 10);
-  }
-
-  async signupLocal(dto: SignupDto) {
+  async signupLocal(dto: SignupDto): Promise<Tokens> {
     const hash = await this.hashData(dto.password);
-    const newUser = this.prismaService.user.create({
+    const newUser = await this.prismaService.user.create({
       data: {
         email: dto.email,
         password: hash,
@@ -21,11 +22,29 @@ export class AuthService {
         lastName: dto.lastName,
       },
     });
-    return 'signed up ' + dto.email;
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
   }
 
-  loginLocal(dto: LoginDto) {
-    return 'logged in ' + dto.email;
+  async loginLocal(dto: LoginDto): Promise<Tokens> {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) throw new ForbiddenException('Incorrect email or password');
+
+    const passwordMatches = await bcrypt.compare(dto.password, user.password);
+    if (!passwordMatches) {
+      throw new ForbiddenException('Incorrect email or password');
+    }
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+    return tokens;
   }
 
   logout() {
@@ -34,5 +53,51 @@ export class AuthService {
 
   refresh() {
     return 0;
+  }
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+    await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
+
+  hashData(data: string): Promise<string> {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const accessToken = this.jwtService.signAsync(
+      {
+        sub: userId,
+        email: email,
+      },
+      {
+        secret: 'AT-STRATEGY-SECRET-KEY',
+        expiresIn: 60 * 15,
+      },
+    );
+
+    const refreshToken = this.jwtService.signAsync(
+      {
+        sub: userId,
+        email: email,
+      },
+      {
+        secret: 'RT-STRATEGY-SECRET-KEY',
+        expiresIn: 60 * 60 * 24 * 7,
+      },
+    );
+
+    const [at, rt] = await Promise.all([accessToken, refreshToken]);
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
   }
 }

e859753dbca985af7ff63b0296fe7f3b2e9ea8cc
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-15 22:07:17 +0500

    feat(auth): added 'AtGuard' and `RtGuard`

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,8 +10,8 @@ import {
 import { AuthService } from './auth.service';
 import { LoginDto, SignupDto } from './dto';
 import { Tokens } from './types';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { AtGuard, RtGuard } from './common/guards';
 
 @Controller('auth')
 export class AuthController {
@@ -29,7 +29,7 @@ export class AuthController {
     return this.authService.loginLocal(dto);
   }
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   logout(@Req() req: Request) {
@@ -37,7 +37,7 @@ export class AuthController {
     return this.authService.logout(user['sub']);
   }
 
-  @UseGuards(AuthGuard('jwt-refresh'))
+  @UseGuards(RtGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   refresh(@Req() req: Request) {

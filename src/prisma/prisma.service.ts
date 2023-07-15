127feaaae6bfceb8aaa54a7e88ad6982bd1fea88
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-15 14:32:16 +0500

    feat(prisma): service is configured properly

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: 'postgresql://postgres:root@localhost:5432/online_store?schema=public',
+        },
+      },
+    });
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$disconnect();
+  }
+}

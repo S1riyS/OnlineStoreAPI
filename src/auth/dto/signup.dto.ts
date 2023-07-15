182603188e5007b69e1b742b864285076c9794fc
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-15 14:52:42 +0500

    feat(auth): added dto validation

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class SignupDto {
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly firstName: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly lastName: string;
 }

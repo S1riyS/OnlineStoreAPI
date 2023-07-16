b71a6ca343b779c78742b869c922124cd63aca6f
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-16 19:31:48 +0500

    feat(swagger): added Swagger documentation endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  // Swagger configuration
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('OnlineStoreAPI')
+    .setDescription('Simple backend for online stores')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(3000);
 }
 

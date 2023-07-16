51ed8ab1ead1b8350e5132d2fe2af61b883e3f80
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-16 22:55:35 +0500

    feat(auth): all endpoints and DTOs are documented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,27 +8,50 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto';
-import { Tokens } from './types';
+import { Tokens, TokensType } from './types';
 import { AtGuard, RtGuard } from '../common/guards';
 import { CurrentUser, CurrentUserId } from '../common/decorators';
 import { CreateUserDto } from '../user/dto';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Registration of new user' })
+  @ApiCreatedResponse({ type: TokensType })
+  @ApiBadRequestResponse({
+    description: 'Email is already used or invalid input data',
+  })
   @Post('local/signup')
   @HttpCode(HttpStatus.CREATED)
   signupLocal(@Body() dto: CreateUserDto): Promise<Tokens> {
     return this.authService.signupLocal(dto);
   }
 
+  @ApiOperation({ summary: 'Log in to the system' })
+  @ApiOkResponse({ type: TokensType })
+  @ApiBadRequestResponse({
+    status: 400,
+    description: 'Invalid username/password supplied or invalid input data',
+  })
   @Post('local/login')
   @HttpCode(HttpStatus.OK)
   loginLocal(@Body() dto: LoginDto): Promise<Tokens> {
     return this.authService.loginLocal(dto);
   }
 
+  @ApiOperation({ summary: 'Current user logout' })
+  @ApiOkResponse({ description: 'Logged out successfully' })
+  @ApiUnauthorizedResponse({ description: 'User is unauthorized' })
   @UseGuards(AtGuard)
   @Post('logout')
   @HttpCode(HttpStatus.OK)
@@ -36,6 +59,9 @@ export class AuthController {
     return this.authService.logout(userId);
   }
 
+  @ApiOperation({ summary: 'Refreshes access and refresh tokens' })
+  @ApiOkResponse({ type: TokensType })
+  @ApiUnauthorizedResponse({ description: 'User is unauthorized' })
   @UseGuards(RtGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
--- src/auth/types/tokens.type.ts
+++ src/auth/types/tokens.type.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export type Tokens = {
   access_token: string;
   refresh_token: string;
 };
+
+export class TokensType implements Tokens {
+  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWI...' })
+  access_token: string;
+
+  @ApiProperty({ example: 'TA5MjQwLCJleHAiOjE2OTAxMTQwNDB9.lrZ6vxmbuMQW...' })
+  refresh_token: string;
+}

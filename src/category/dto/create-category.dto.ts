e6f35f7493e9e08d5f524d5f7b060615b0c50019
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-17 14:39:00 +0500

    feat(prisma): parentId of category is optional

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,4 +1,11 @@
+import { IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreateCategoryDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsInt()
+  @IsOptional()
   parentId?: number;
 }

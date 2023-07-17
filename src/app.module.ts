c7dc1441465183f9e666cd725689e581f639ecd9
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-17 12:36:14 +0500

    feat(item): added template of `item` module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule],
+  imports: [UserModule, AuthModule, PrismaModule, ItemModule],
 })
 export class AppModule {}

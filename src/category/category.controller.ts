98420fb3caa88a40bf2ff44fa9754fe723d46a46
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-19 22:54:05 +0500

    fix(category): changed order of endpoints

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -55,14 +55,6 @@ export class CategoryController {
     return this.categoryService.create(dto);
   }
 
-  @ApiOperation({ summary: 'Returns category with given ID' })
-  @ApiOkResponse({ type: CategoryEntity })
-  @ApiNotFoundResponse({ description: 'Category not found', type: ApiError })
-  @Get(':id')
-  getOneById(@Param('id', ParseIntPipe) categoryId: number) {
-    return this.categoryService.getOneByID(categoryId);
-  }
-
   @ApiOperation({ summary: 'Returns full tree of categories' })
   @ApiOkResponse({ type: [CategoryEntity] })
   @Get('tree')
@@ -76,5 +68,13 @@ export class CategoryController {
   @Get('tree/:id')
   getTreeById(@Param('id', ParseIntPipe) categoryId: number) {
     return this.categoryService.getTreeById(categoryId);
+  }
+
+  @ApiOperation({ summary: 'Returns category with given ID' })
+  @ApiOkResponse({ type: CategoryEntity })
+  @ApiNotFoundResponse({ description: 'Category not found', type: ApiError })
+  @Get(':id')
+  getOneById(@Param('id', ParseIntPipe) categoryId: number) {
+    return this.categoryService.getOneByID(categoryId);
   }
 }

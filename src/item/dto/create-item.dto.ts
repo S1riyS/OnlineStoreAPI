06990f06f1d7b7bcc402f97fa4f730dbe83f8e09
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-20 21:03:18 +0500

    feat(swagger): `CreateItemDto` and `AdditionalProp` are documented

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -10,30 +10,39 @@ import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 class AdditionalProp {
+  @ApiProperty({ example: 2 })
   @IsNotEmpty()
   @Type(() => Number)
   @IsInt()
   additionalPropId: number;
 
+  @ApiProperty({ example: 'Socket AM4' })
   @IsNotEmpty()
   @IsString()
   value: string;
 }
 
 export class CreateItemDto {
+  @ApiProperty({ example: 'AMD Ryzen 5 5600X BOX' })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   description: string;
 
+  @ApiProperty({ example: 15999 })
   @IsNotEmpty()
   @Type(() => Number)
   @IsInt()
   price: number;
 
+  @ApiProperty({ type: 'string', format: 'binary' })
+  image: Express.Multer.File;
+
+  @ApiProperty({ example: 2 })
   @IsNotEmpty()
   @Type(() => Number)
   @IsInt()

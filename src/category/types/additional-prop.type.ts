78f458fc1700b998595b9d26f118c45fcc63a15c
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-22 00:43:34 +0500

    refactor(swagger): changed examples of `AdditionalPropValue` and `GetCategoryPropsResponse`

--- src/category/types/additional-prop.type.ts
+++ src/category/types/additional-prop.type.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 class AdditionalPropValue {
-  @ApiProperty({ example: '4 kg' })
+  @ApiProperty({ example: '4.6 GHz' })
   value: string;
 
   @ApiProperty({ example: 5 })
@@ -12,7 +12,7 @@ export class GetCategoryPropsResponse {
   @ApiProperty({ example: 1 })
   id: number;
 
-  @ApiProperty({ example: 'Weight' })
+  @ApiProperty({ example: 'CPU Speed' })
   name: string;
 
   @ApiProperty({ type: [AdditionalPropValue] })

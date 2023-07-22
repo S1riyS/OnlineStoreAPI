d3685a5d44e9e50cad5699c3d3bafb494b9f21e7
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2023-07-22 19:52:43 +0500

    feat(item): added response types

---
+++ src/cart/types/cart.type.ts
@@ -0,0 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { GetItemSimplifiedResponse } from '../../item/types';
+
+class CartItem {
+  @ApiProperty()
+  item: GetItemSimplifiedResponse;
+
+  @ApiProperty()
+  counter: number;
+}
+
+class Cart {
+  @ApiProperty({ example: 6 })
+  id: number;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty({ example: 2 })
+  userId: number;
+
+  @ApiProperty({ type: [CartItem] })
+  items: CartItem[];
+}
+
+export class CreateCartResponse {
+  @ApiProperty({ example: 6, description: 'ID of cart' })
+  id: number;
+}
+
+export class GetCartResponse extends Cart {}
+
+export class AddCartItemResponse extends Cart {}
+
+export class UpdateCartItemResponse extends Cart {}
